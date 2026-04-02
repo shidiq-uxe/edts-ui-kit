6a650995970c9d878b353639ec1a628b3b6a5379
Shidiq Bagus Ardi Prastian <shidiq.bagus@sg-edts.com>
2026-04-02 16:12:31 +0700

    improvement: added single item for coachmark

--- uikit/src/main/java/id/co/edtslib/uikit/coachmark/CoachmarkDelegate.kt
+++ uikit/src/main/java/id/co/edtslib/uikit/coachmark/CoachmarkDelegate.kt
@@ -1,7 +1,7 @@
 package id.co.edtslib.uikit.coachmark
 
 interface CoachmarkDelegate {
-    fun onNextClickClickListener()
-    fun onSkipClickListener()
+    fun onNextClickClickListener(currentIndex: Int)
+    fun onSkipClickListener(currentIndex: Int)
     fun onDismissListener()
 }
